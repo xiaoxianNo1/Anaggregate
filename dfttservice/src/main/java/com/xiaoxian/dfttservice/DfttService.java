8a3b5efd4a21c7acba3c17f7b8167ad206ef1cf7
xiaoxianNo1 <1836519433@139.com>
2019-02-14 16:49:40 +0800

    东方头条初次提交

---
+++ dfttservice/src/main/java/com/xiaoxian/dfttservice/DfttService.java
@@ -0,0 +1,21 @@
+package com.xiaoxian.dfttservice;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * 简介：东方头条服务
+ * 作者：郑现文
+ * 创建时间：2019/2/11/0011 09:30
+ **/
+public class DfttService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
---
+++ dfttservice/src/main/java/com/xiaoxian/dfttservice/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xiaoxian.dfttservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
